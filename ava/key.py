82b3ed5d6b8fbe2fc496cb94e8e16f0566eefed3
sampot <sam@eavatar.com>
2015-02-26 21:08:31 +0800

    public and secret keys use verifiable format

--- ava/key.py
+++ ava/key.py
@@ -11,7 +11,9 @@ import base58
 import libnacl.public
 import libnacl.secret
 
-XID_PREFIX = b'\xea'
+XID_PREFIX = b'\x5f'
+KEY_PREFIX = b'\xba'  # prefix for public key string
+SECRET_PREFIX = b'\xff'  # prefix for secret key string
 
 
 def generate_keypair(sk=None):
@@ -85,12 +87,141 @@ def derive_secret_key(password, salt):
     return sk
 
 
+def secret_to_string(key):
+    """
+    Converts a secret key to its string form
+    :param key: the secret key
+    :return: a string equivalent to the secret key
+    """
+    sha256 = hashlib.sha256()
+    sha256.update(SECRET_PREFIX)
+    sha256.update(key)
+    sum = sha256.digest()
+    result = SECRET_PREFIX + key + sum[:2]
+
+    return base58.b58encode(result)
+
+
+def string_to_secret(sk_str):
+    val = base58.b58decode(sk_str)
+
+    if len(val) != 35:
+        return None
+
+    prefix = val[0]
+    if prefix != SECRET_PREFIX:
+        return None
+
+    key = val[1:33]
+    sha256 = hashlib.sha256()
+    sha256.update(SECRET_PREFIX)
+    sha256.update(key)
+    s = sha256.digest()
+
+    if val[-2:] != s[:2]:
+        return None
+
+    return key
+
+
+def validate_secret_string(sk_str):
+    """
+    Validates a given public key string.
+    :param sk_str:
+    :return:
+    """
+    val = base58.b58decode(sk_str)
+
+    if len(val) != 35:
+        return False
+
+    prefix = val[0]
+    if prefix != SECRET_PREFIX:
+        return False
+
+    sha256 = hashlib.sha256()
+    sha256.update(SECRET_PREFIX)
+    sha256.update(val[1:33])
+    s = sha256.digest()
+
+    if val[-2:] != s[:2]:
+        return False
+
+    return True
+
+
+def key_to_string(key):
+    """
+    Converts a public key to its string form
+    :param key: the public key
+    :return:
+    """
+    sha256 = hashlib.sha256()
+    sha256.update(KEY_PREFIX)
+    sha256.update(key)
+    sum = sha256.digest()
+    result = KEY_PREFIX + key + sum[:2]
+
+    return base58.b58encode(result)
+
+
+def string_to_key(pk_str):
+    val = base58.b58decode(pk_str)
+
+    if len(val) != 35:
+        return None
+
+    prefix = val[0]
+    if prefix != KEY_PREFIX:
+        return None
+
+    key = val[1:33]
+    sha256 = hashlib.sha256()
+    sha256.update(KEY_PREFIX)
+    sha256.update(key)
+    s = sha256.digest()
+
+    if val[-2:] != s[:2]:
+        return None
+
+    return key
+
+
+def validate_key_string(pk_str):
+    """
+    Validates a given public key string.
+    :param sk_str:
+    :return:
+    """
+    val = base58.b58decode(pk_str)
+    #assert len(val) == 35
+
+    if len(val) != 35:
+        return False
+
+    prefix = val[0]
+    if prefix != KEY_PREFIX:
+        return False
+
+    sha256 = hashlib.sha256()
+    sha256.update(KEY_PREFIX)
+    sha256.update(val[1:33])
+    s = sha256.digest()
+
+    if val[-2:] != s[:2]:
+        return False
+
+    return True
+
+
 def gen_random_key(ctx):
     if ctx.obj['verbose']:
         click.echo("Generating random key...")
     (pk, sk) = generate_keypair()
     xid = key_to_xid(pk)
-    res = dict(xid=xid, key=base58.b58encode(pk), secret=base58.b58encode(sk))
+    secret_str = secret_to_string(sk)
+    key_str = key_to_string(pk)
+    res = dict(xid=xid, key=key_str, secret=secret_str)
     click.echo(json.dumps(res, sort_keys=True, indent=4, separators=(',', ': ')))
     return 0
 
@@ -123,7 +254,9 @@ def generate(ctx, salt, password):
     sk = derive_secret_key(password, salt)
     (pk, sk) = generate_keypair(sk=sk)
     xid = key_to_xid(pk)
-    res = dict(xid=xid, key=base58.b58encode(pk), secret=base58.b58encode(sk))
+    secret_str = secret_to_string(sk)
+    key_str = key_to_string(pk)
+    res = dict(xid=xid, key=key_str, secret=secret_str)
     click.echo(json.dumps(res, sort_keys=True, indent=4, separators=(',', ': ')))
     return 0
 
@@ -143,19 +276,28 @@ def validate(ctx, xid, key, secret):
         else:
             click.echo("XID %s is invalid." % xid)
 
+    if key:
+        if validate_key_string(key):
+            click.echo("Key is valid")
+        else:
+            click.echo("Key is invalid.")
+
+    if secret:
+        if validate_secret_string(secret):
+            click.echo("Secret is valid.")
+        else:
+            click.echo("Secret is invalid.")
+
     if key and secret:
-        sk = base58.b58decode(secret)
-        pk = base58.b58decode(key)
+        sk = string_to_secret(secret)
+        pk = string_to_key(key)
         (vpk, vsk) = generate_keypair(sk=sk)
         if vpk == pk and vsk == sk:
-            click.echo("Key and secret are valid and matched.")
+            click.echo("Key and secret are matched.")
             return 0
-        click.echo("Key or secret is invalid or mismatched.")
+        click.echo("Key and secret are mismatched.")
         return 1
 
-    if key or secret:
-        click.echo("Options key and secret should be given together.")
-        return 1
 
 if __name__ == '__main__':
     sys.exit(cli())
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(fname):
 
 setup(
     name="ava-key",
-    version="0.1.3",
+    version="0.2.0",
     description="Key generator and validator.",
     zip_safe=True,
     author='Sam Kuo',
