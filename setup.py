b6b5c7e38e5fa12706f34f6c674e5728ebe412cd
sampot <sam@eavatar.com>
2015-02-26 11:11:26 +0800

    refactor for PyPi publishing

--- setup.py
+++ setup.py
@@ -7,25 +7,42 @@ Usage:
     python setup.py build
 """
 
+import os
+from setuptools import setup
 
-from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="ava-key",
-    version="0.1.0",
+    name="ava.key",
+    version="0.1.1",
     description="Key generator and validator.",
-    include_package_data=True,
     zip_safe=True,
-    packages=find_packages(),
+    author='Sam Kuo',
+    author_email='sam@eavatar.com',
+    license='BSD',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Operating System :: OS Independent",
+    ],
+    packages=['ava'],
+    long_description=read('README.rst'),
     install_requires=[
-        'Click',
-        'libnacl',
-        'base58',
-        'pyscrypt'
+        "base58>=0.2.1",
+        "click>=3.3",
+        "libnacl>=1.4.1",
+        "pyscrypt>=1.6.2",
     ],
     entry_points={
-        'console-scripts': [
-            'ava-key = ava-key:cli',
+        'console_scripts': [
+            'ava-key = ava.key:cli',
         ]
     }
 )
